aaa863b65629cfec4800917b1e401c53a11e6c51
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2021-07-13 17:14:48 +0100

    contact form functionalities added

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// variables
+var contactForm = document.querySelector('.form');
+var user = document.querySelector('.name');
+var subject = document.querySelector('.subject');
+var message = document.querySelector('.message');
+var iconOpen = document.querySelector('.icon__open');
+var iconClose = document.querySelector('.icon__close');
+var menuList = document.querySelector('.nav');
+// function to open mobile nav
+iconOpen.addEventListener('click', function () {
+    console.log("clicked");
+    menuList.style.left = '0';
+    iconOpen.style.display = 'none';
+    iconClose.style.display = 'block';
+});
+// function to close mobile nav
+iconClose.addEventListener('click', function () {
+    console.log("clicked");
+    menuList.style.left = '-100%';
+    iconClose.style.display = 'none';
+    iconOpen.style.display = 'block';
+});
+// function to submit contact form
+contactForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if ((user.value == '') && (subject.value == '') && (message.value == '')) {
+        console.log("Fields are empty!!");
+    }
+    else {
+        console.log('Name:', user.value);
+        console.log('Subject:', subject.value);
+        console.log('Message:', message.value);
+    }
+});
---
+++ js/app.ts
@@ -0,0 +1,34 @@
+// variables
+const contactForm = document.querySelector('.form');
+const user = document.querySelector('.name') as HTMLInputElement;
+const subject = document.querySelector('.subject') as HTMLInputElement;
+const message = document.querySelector('.message') as HTMLTextAreaElement;
+const iconOpen = document.querySelector('.icon__open') as HTMLImageElement;
+const iconClose = document.querySelector('.icon__close') as HTMLImageElement;
+const menuList = document.querySelector('.nav') as HTMLElement;
+// function to open mobile nav
+iconOpen.addEventListener('click', ()=>{
+    console.log("clicked");
+    menuList.style.left = '0';
+    iconOpen.style.display = 'none';
+    iconClose.style.display = 'block';
+})
+// function to close mobile nav
+iconClose.addEventListener('click', ()=>{
+    console.log("clicked");
+    menuList.style.left = '-100%';
+    iconClose.style.display = 'none';
+    iconOpen.style.display = 'block';
+})
+// function to submit contact form
+contactForm.addEventListener('submit', (e: Event)=>{
+    e.preventDefault()
+    if((user.value == '') && (subject.value == '') && (message.value == '')){
+        console.log("Fields are empty!!");
+    }
+    else{
+        console.log('Name:', user.value);
+        console.log('Subject:', subject.value);
+        console.log('Message:', message.value);
+    }
+})
